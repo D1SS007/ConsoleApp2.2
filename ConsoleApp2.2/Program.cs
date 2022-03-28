d4394669b9a90ddbb2f91b766c554c637df0969c
Alex Zverev <lordzver007@yandex.ru>
2022-03-28 15:52:33 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stopWord = "exit";
+            string userInput;
+
+            while(true)
+            {
+                Console.WriteLine("Введите стоп слово");
+                userInput = Console.ReadLine();
+                
+                if (   userInput == stopWord)
+                {
+                    Console.WriteLine("Программа окончена");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Программа продолжается");
+                }
+            }                           
+        }
+    }
+}
